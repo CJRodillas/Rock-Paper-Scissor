c31c2f9e7cb234662617d95cda102c40277910e7
CJRodillas <mjclores10@gmail.com>
2022-04-08 16:44:48 +0800

    add the loop in the playRound function

--- script/script.js
+++ script/script.js
@@ -1,13 +1,37 @@
-console.log("Hello World");
 
-let computerSelection;
+let computerSelection = computerPlay();
+let playerSelection = prompt("Please choose between Rock, Paper, Scissors: ");;
 
 function computerPlay() {
-       let arr = ["Rock", "Paper", "Scissors"];
+       let arr = ["Paper", "Rock", "Scissors"];
 
-       computerSelection = arr[Math.floor(Math.random() * arr.length)];
+       arr = arr[Math.floor(Math.random() * arr.length)];
 
-       console.log(computerSelection);
+       return arr;
 };
 
-computerPlay();
+function playRound(computerSelection, playerSelection) {
+       
+       if (computerSelection == "Paper" &&  playerSelection == "Rock") {
+              console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+       } else if (computerSelection == "Paper" &&  playerSelection == "Scissors") {
+              console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+       } else if (computerSelection == "Paper" &&  playerSelection == "Paper") {
+              console.log(`Draw! you both play ${computerSelection}`);
+       }  else if (computerSelection == "Rock" &&  playerSelection == "Scissors") {
+              console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+       } else if (computerSelection == "Rock" &&  playerSelection == "Paper") {
+              console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+       } else if (computerSelection == "Rock" &&  playerSelection == "Rock") {
+              console.log(`Draw! you both play ${computerSelection}`);
+       } else if (computerSelection == "Scissors" &&  playerSelection == "Paper") {
+              console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+       } else if (computerSelection == "Scissors" &&  playerSelection == "Rock") {
+              console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+       } else if (computerSelection == "Scissors" &&  playerSelection == "Scissors") {
+              console.log(`Draw! you both play ${computerSelection}`);
+       }
+};
+
+playRound(computerSelection, playerSelection);
+
