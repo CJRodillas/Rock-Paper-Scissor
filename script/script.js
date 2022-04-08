e8db830762a67260c5bd1a4e1c8cad38e10439bc
CJRodillas <mjclores10@gmail.com>
2022-04-08 16:12:41 +0800

    add function of computerPlay with its arr and randomizer

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
 console.log("Hello World");
 
-function computerPlay {
-       
-}
+function computerPlay() {
+       let arr = ["Rock", "Paper", "Scissors"];
+
+       let result = arr[Math.floor(Math.random() * arr.length)];
+
+       console.log(result);
+};
+
+computerPlay();
